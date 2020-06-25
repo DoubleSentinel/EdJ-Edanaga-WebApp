85cce87671eea5656074ad19e5890e01dfc0f4f7
Karim Luy <karim.luy@he-arc.ch>
2020-06-25 15:10:20 +0200

    created rest api core files including Docker and pipenv

---
+++ rest/rest.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, url_for, abort, request
+
+from flask_mongoengine import MongoEngine
+from mongoengine import errors as mongoerrors
+
+from flask_admin import Admin, helpers
+
+from flask_restx import Api, Resource, fields
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "This is the api interface"
+
