46ec6653cf582bd48fa2b595821802df983e1388
Karim Luy <karim.luy@he-arc.ch>
2020-06-25 15:11:32 +0200

    removed unnecessary dependency

--- rest/rest.py
+++ rest/rest.py
@@ -3,8 +3,6 @@ from flask import Flask, render_template, redirect, url_for, abort, request
 from flask_mongoengine import MongoEngine
 from mongoengine import errors as mongoerrors
 
-from flask_admin import Admin, helpers
-
 from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
