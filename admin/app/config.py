34509d155d00f9155fbc8c7651e416347b9cb91e
Karim Luy <karim.luy@he-arc.ch>
2020-07-09 14:58:50 +0200

    added configuration file to admin as well as updated docker-compose to forward configuration data

---
+++ admin/app/config.py
@@ -0,0 +1,21 @@
+import os
+
+MONGODB_SETTINGS = {
+    'db': os.environ.get("MONGODB"),
+    'username': os.environ.get("MONGOUSER"),
+    'password': os.environ.get("MONGOPASS"),
+    'host': os.environ.get("MONGOHOST")
+}
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
+SECURITY_PASSWORD_SALT = os.environ.get("SECURITY_PASSWORD_SALT")
+
+SECURITY_CHANGEABLE = True
+SECURITY_SEND_PASSWORD_CHANGE_EMAIL = False
+SECURITY_CHANGE_URL = '/change'
+SECURITY_LOGIN_URL = '/login'
+SECURITY_LOGOUT_URL = '/logout'
+SECURITY_POST_LOGIN_VIEW = '/'
+SECURITY_POST_LOGOUT_VIEW = '/login'
+
