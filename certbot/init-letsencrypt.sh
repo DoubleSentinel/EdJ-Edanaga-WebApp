cdea6727fd9c5b16b80339e230ee803d7ce1dbff
DoubleSentinel <contact@karimluy.ch>
2020-07-21 15:50:18 +0200

    changed root directory for init script

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(edanaga.ch www.edanaga.ch, api.edanaga.ch, admin.edanaga.ch)
 rsa_key_size=4096
-data_path="./certbot"
+data_path="."
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
