2e6c2a2fe55a6ebddd27ec8687ddd1c79f787722
DoubleSentinel <contact@karimluy.ch>
2020-07-21 17:12:30 +0200

    reconfigured nginx to serve properly, hopefully

--- certbot/init-letsencrypt.sh
+++ certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(edanaga.ch www.edanaga.ch, api.edanaga.ch, admin.edanaga.ch)
+domains=(edanaga.ch www.edanaga.ch api.edanaga.ch admin.edanaga.ch)
 rsa_key_size=4096
 data_path="."
 email="" # Adding a valid address is strongly recommended
