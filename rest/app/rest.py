53531a0b1f23a2820c5b6f526f061d97464f78f9
DoubleSentinel <contact@karimluy.ch>
2020-08-17 16:38:08 +0200

    added an endpoint to update the invitation participants list

--- rest/app/rest.py
+++ rest/app/rest.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, request, jsonify
 
 from flask_mongoengine import MongoEngine
@@ -6,7 +7,7 @@ from mongoengine import errors as mongoerrors
 from flask_mongorest import MongoRest, methods
 from flask_mongorest.views import ResourceView
 
-from models import TestUser
+from models import TestUser, Invitations
 
 from resources import (LanguagesResource,
                        SceneResource,
@@ -50,7 +51,7 @@ class TestUserView(ResourceView):
     methods = [methods.Create, methods.Update]
 
 
-@api.register(name='isuser', url='/is_user/')
+@api.register(name='is_user', url='/is_user/')
 class IsUserView(ResourceView):
     resource = IsUserResource
     methods = [methods.List]
@@ -59,7 +60,9 @@ class IsUserView(ResourceView):
 @app.route("/login_user/", methods=['POST'])
 def login():
     try:
-        if TestUser.objects.get(username=request.json["username"]).userpass == request.json["userpass"]:
+        user = TestUser.objects.get(username=request.json["username"])
+        if user.userpass == request.json["userpass"]:
+            user.last_login = datetime.datetime.utcnow()
             return jsonify(error={"code": 200, "message": "ok"})
         else:
             return jsonify(error={"code": 400, "message": "wrong password"})
@@ -68,3 +71,14 @@ def login():
     except mongoerrors.DoesNotExist:
         return jsonify(error={"code": 400, "message": "Username does not exist"})
 
+
+@app.route("/update_invite/", methods=['POST'])
+def update_invite():
+    try:
+        token = Invitations.objects.get(token_url=request.json["token_url"])
+        token.update(add_to_set__participants=request.json["user_id"])
+        return jsonify(error={"code": 200, "message": "ok"})
+    except KeyError:
+        return jsonify(error={"code": 400, "message": "Bad request"})
+    except mongoerrors.DoesNotExist:
+        return jsonify(error={"code": 400, "message": "Token does not exist"})
